3d9f022e2fa1b78baebb917cb0da6c6ee8cfff73
yshir <yshir0020@gmail.com>
2020-10-11 20:39:16 +0900

    remove-todo

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { TodoContext } from '../contexts/TodoContext';
 import { TodoType } from '../types/Todo';
 
 interface IProps {
@@ -7,11 +8,18 @@ interface IProps {
 
 export const Todo: React.FC<IProps> = props => {
   const { todo } = props;
+  const { dispatch } = useContext(TodoContext);
+
+  const removeTodoHandler = () => {
+    dispatch({ type: 'REMOVE_TODO', payload: { id: todo.id } });
+  };
+
   return (
     <>
       <div style={{ padding: '6px' }}>
         <p>{todo.id}</p>
         <p>{todo.text}</p>
+        <button onClick={removeTodoHandler}>Remove</button>
       </div>
     </>
   );
